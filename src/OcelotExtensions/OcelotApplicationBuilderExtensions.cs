e20c082eebf4c68a9e59dd74b31d3886e2596197
codeping <deping.du@gmail.com>
2020-07-06 00:51:10 +0800

    完成初版。

---
+++ src/OcelotExtensions/OcelotApplicationBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Ocelot.Middleware;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    public static class OcelotApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseOcelot(this IApplicationBuilder builder)
+        {
+            OcelotMiddlewareExtensions.UseOcelot(builder).Wait();
+
+            return builder;
+        }
+    }
+}
---
+++ src/OcelotExtensions/OcelotServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Ocelot.Configuration.File;
+using Ocelot.DependencyInjection;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class OcelotServiceCollectionExtensions
+    {
+        public static IServiceCollection AddOcelot(
+            this IServiceCollection services, Action<FileConfiguration> configureBinder)
+        {
+            services.Configure(configureBinder);
+
+            services.AddOcelot();
+
+            return services;
+        }
+    }
+}
